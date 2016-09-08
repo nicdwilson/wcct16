4346b40e05adcddb11b8a011c736eedf16248076
Nic <nic.d.wilson@gmail.com>
2016-09-08 11:53:37 +0200

    Update test-unit-sample.php

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -15,7 +15,16 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	}
 	
 	public function test_wcct_get_option(){
+		
+		$get_option = $this->getFunctionMock('WCCT16', get_option );
+		
+		$get_option->expects($this->once())
+		->with($this->equalTo('wcct_venue'))
+		->willReturn('river-club');
+		
 		$this->assertEquals('river-club', wcct_get_option('venue'));
 	}
+	
+	
 
 }
